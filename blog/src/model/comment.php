f17d5882603e61fed845b4a55be29d9e4f4a5eac
ethan <ethan.hugerot@hotmail.fr>
2024-10-22 11:10:30 +0200

    Structurez les données des commentaires

--- blog/src/model/comment.php
+++ blog/src/model/comment.php
@@ -1,4 +1,10 @@
 <?php
+class Comment
+{
+    public string $author;
+    public string $frenchCreationDate;
+    public string $comment;
+}
 
 function getComments(string $post)
 {
@@ -7,18 +13,14 @@ function getComments(string $post)
         "SELECT id, author, comment, DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM comments WHERE post_id = ? ORDER BY comment_date DESC"
     );
     $statement->execute([$post]);
-
     $comments = [];
     while (($row = $statement->fetch())) {
-        $comment = [
-            'author' => $row['author'],
-            'french_creation_date' => $row['french_creation_date'],
-            'comment' => $row['comment'],
-        ];
-
+        $comment = new Comment();
+        $comment->author = $row['author'];
+        $comment->frenchCreationDate = $row['french_creation_date'];
+        $comment->comment = $row['comment'];
         $comments[] = $comment;
     }
-
     return $comments;
 }
 
@@ -29,13 +31,11 @@ function createComment(string $post, string $author, string $comment)
         'INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())'
     );
     $affectedLines = $statement->execute([$post, $author, $comment]);
-
     return ($affectedLines > 0);
 }
 
 function commentDbConnect()
 {
     $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'blog', 'password');
-
     return $database;
 }
